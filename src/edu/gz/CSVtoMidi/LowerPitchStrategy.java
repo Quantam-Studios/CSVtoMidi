a5fc51dce750a411ba96187aedef2212052e22c0
Gradon Burns <gradyhburns@gmail.com>
2025-04-02 21:36:47 -0500

    Updated LowerPitchStrategy to implement PitchStrategy interface

--- src/edu/gz/CSVtoMidi/LowerPitchStrategy.java
+++ src/edu/gz/CSVtoMidi/LowerPitchStrategy.java
@@ -4,7 +4,7 @@ package edu.gz.CSVtoMidi;
  * This class implements a strategy for modifying the pitch of a MIDI note by decreasing it.
  * It subtracts 2 to the given note value, representing a shift to a lower pitch.
  */
-public class LowerPitchStrategy {
+public class LowerPitchStrategy implements PitchStrategy {
 
     /**
      * Modifies the pitch of the given MIDI note by decreasing it by 2.
