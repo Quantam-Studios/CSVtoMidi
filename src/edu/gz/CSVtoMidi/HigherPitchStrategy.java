d99b6f1617e4e8e9f7f6cfcec2db6b754c07108d
Gradon Burns <gradyhburns@gmail.com>
2025-04-02 21:36:18 -0500

    Updated HigherPitchStrategy to implement PitchStrategy interface

--- src/edu/gz/CSVtoMidi/HigherPitchStrategy.java
+++ src/edu/gz/CSVtoMidi/HigherPitchStrategy.java
@@ -4,7 +4,7 @@ package edu.gz.CSVtoMidi;
  * This class implements a strategy for modifying the pitch of a MIDI note by increasing it.
  * It adds 2 to the given note value, representing a shift to a higher pitch.
  */
-public class HigherPitchStrategy {
+public class HigherPitchStrategy implements PitchStrategy {
 
     /**
      * Modifies the pitch of the given MIDI note by increasing it by 2.
